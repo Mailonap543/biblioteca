82d921ee5861c943ad7956c2bc09aecc2d57181b
Mailonap543 <87220875+Mailonap543@users.noreply.github.com>
2023-09-15 13:05:41 +0000

    Funcionario

---
+++ src/biblioteca/Funcionario.java
@@ -0,0 +1,47 @@
+public class {
+              
+ String nome;
+ long cpf;
+ String cargo;
+ double salario;
+ Endereco objEndereco;
+
+  public Funcionario() {
+  this.objEndereco = new Endereco();
+}
+
+public String getNome(){
+  return nome;
+}
+
+public void setNome(String nome){
+  this.nome = nome 
+}
+
+public String getCargo (){
+  return nome;
+}
+
+public void setCargo (String cargo){
+  this.cargo =cargo; 
+}
+
+public long getCpf (){
+  retunr cpf;
+}
+
+public void setCpf(long cpf){
+  this.cpf = cpf
+  
+} 
+  
+public double getSalario (){
+  return salario;
+  
+}
+
+public void setSalario(){
+  this.salario = salario
+ }
+
+
