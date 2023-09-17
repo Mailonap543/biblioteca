0c4791a2d86cec6104d2bd46856d226275a138a5
Mailonap543 <87220875+Mailonap543@users.noreply.github.com>
2023-09-17 00:38:27 +0000

    Usuario.java

---
+++ src/biblioteca/Usuario.java
@@ -0,0 +1,48 @@
+public class Usuario {
+  
+    String nome;
+    int codigo;
+    long cpf;
+    Endereco objEndereco;
+
+public Usuario() {
+  this.objEndereco = new Endereco();
+}
+
+public getUsuario() {
+  this.usurio = new usurio();
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public int getCodigo() {
+	return codigo;
+}
+
+public void setCodigo(int codigo) {
+	this.codigo = codigo;
+}
+
+public long getCpf() {
+	return cpf;
+}
+
+public void setCpf(long cpf) {
+	this.cpf = cpf;
+}
+
+public Endereco getObjEndereco() {
+	return objEndereco;
+}
+
+public void setObjEndereco(Endereco objEndereco) {
+	this.objEndereco = objEndereco;
+}
+
+}
